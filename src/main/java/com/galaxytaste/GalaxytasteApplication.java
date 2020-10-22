fe12a1147ba7bb2c51a357641b73701db5b487a3
Dinh Phu <ndp17hp@gmail.com>
2020-10-22 13:57:14 +0700

    first commit

---
+++ src/main/java/com/galaxytaste/GalaxytasteApplication.java
@@ -0,0 +1,13 @@
+package com.galaxytaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxytasteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalaxytasteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/galaxytaste/GalaxytasteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galaxytaste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalaxytasteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

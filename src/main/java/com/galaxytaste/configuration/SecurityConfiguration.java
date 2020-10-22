4acfaf412638c31d54f0a03e0d7b59146153f15a
Dinh Phu <ndp17hp@gmail.com>
2020-10-22 16:51:59 +0700

    xong security

--- src/main/java/com/galaxytaste/configuration/SecurityConfiguration.java
+++ src/main/java/com/galaxytaste/configuration/SecurityConfiguration.java
@@ -1,38 +1,68 @@
 package com.galaxytaste.configuration;
 
+import com.galaxytaste.constant.SecurityConstant;
 import com.galaxytaste.filter.JwtAccessDeniedHandler;
 import com.galaxytaste.filter.JwtAuthenticationEntryPoint;
 import com.galaxytaste.filter.JwtAuthorizationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+    @Autowired
     private JwtAuthorizationFilter jwtAuthorizationFilter;
-
+    @Autowired
     private JwtAccessDeniedHandler jwtAccessDeniedHandler;
-
+    @Autowired
     private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-
-
+    @Autowired
+    @Qualifier("UserDetailsService")
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.userDetailsService(this.userDetailsService).passwordEncoder(this.bCryptPasswordEncoder);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable().cors().and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(SecurityConstant.PUBLIC_URLS)
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .exceptionHandling()
+                .accessDeniedHandler(this.jwtAccessDeniedHandler)
+                .authenticationEntryPoint(this.jwtAuthenticationEntryPoint)
+                .and()
+                .addFilterBefore(this.jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Bean

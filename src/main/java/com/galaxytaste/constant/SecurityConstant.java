44231a92fec9b8a1f344896b59b39d735069026f
Dinh Phu <ndp17hp@gmail.com>
2020-10-22 14:53:42 +0700

    xong JWTTokenProvider and SecurityConstant

---
+++ src/main/java/com/galaxytaste/constant/SecurityConstant.java
@@ -0,0 +1,15 @@
+package com.galaxytaste.constant;
+
+public class SecurityConstant {
+    public static final long EXPIRATION_TIME = 432_000_000;// 5 days expressed in milliseconds
+    public static final String TOKEN_PREFIX = "Galaxy ";
+    public static final String JWT_TOKEN_HEADER = "Jwt-Token";
+    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
+    public static final String GET_ARRAYS_LLC = "GalaxyTaste LLC";
+    public static final String GET_ARRAYS_ADMINISTRATION = "Made by Dinh Phu";
+    public static final String AUTHORITIES = "authorities";
+    public static final String FORBIDDEN_MESSAGE = "You need to log in to access this page";
+    public static final String ACCESS_DENIED_MESSAGE = "you do not have permission to access this page";
+    public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
+    public static final String[] PUBLIC_URLS = {"/user/login", "/user/register", "/user/resetpassword/**", "/user/image/**"};
+}
---
+++ src/main/java/com/galaxytaste/utilities/JWTTokenProvider.java
@@ -0,0 +1,100 @@
+package com.galaxytaste.utilities;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.galaxytaste.domain.UserPrincipal;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.galaxytaste.constant.SecurityConstant.*;
+
+@Component
+public class JWTTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secret; // very secure
+
+    public String generateJwtToken(UserPrincipal userPrincipal) {
+        String[] claims = getClaimsFromUser(userPrincipal);
+        return JWT.create()
+                .withIssuer(GET_ARRAYS_LLC)
+                .withAudience(GET_ARRAYS_ADMINISTRATION)
+                .withIssuedAt(new Date())
+                .withSubject(userPrincipal.getUsername())
+                .withArrayClaim(AUTHORITIES, claims)
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(Algorithm.HMAC512(secret.getBytes()))
+                ;
+    }
+
+    public List<GrantedAuthority> getAuthorities(String token) {
+        String[] claims = getClaimsFromToken(token);
+        return Arrays.stream(claims).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    public Authentication getAuthentication(String username, List<GrantedAuthority> authorities, HttpServletRequest request) {
+        UsernamePasswordAuthenticationToken userPasswordAuthToken = new
+                UsernamePasswordAuthenticationToken(username, null, authorities);
+        userPasswordAuthToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        return userPasswordAuthToken;
+    }
+
+    public boolean isTokenValid(String username, String token){
+        JWTVerifier verifier=getJWTVerifier();
+        return StringUtils.isNotEmpty(username) && !isTokenExpired(verifier,token);
+    }
+
+    public String getSubject(String token){
+        JWTVerifier verifier=getJWTVerifier();
+        return verifier.verify(token).getSubject();
+    }
+
+    private boolean isTokenExpired(JWTVerifier verifier, String token) {
+        Date expiration=verifier.verify(token).getExpiresAt();
+        return expiration.before(new Date());
+    }
+
+    private String[] getClaimsFromToken(String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return verifier.verify(token).getClaim(AUTHORITIES).asArray(String.class);
+    }
+
+    private JWTVerifier getJWTVerifier() {
+        JWTVerifier verifier;
+        try {
+            Algorithm algorithm = Algorithm.HMAC512(secret);
+            verifier = JWT.require(algorithm)
+                    .withIssuer(GET_ARRAYS_LLC)
+                    .build();
+        } catch (JWTVerificationException exception) {
+            throw new JWTVerificationException(TOKEN_CANNOT_BE_VERIFIED);
+        }
+        return verifier;
+    }
+
+    private String[] getClaimsFromUser(UserPrincipal user) {
+        List<String> authorities = new ArrayList<>();
+
+        for (GrantedAuthority grantedauthority : user.getAuthorities()) {
+            authorities.add(grantedauthority.getAuthority());
+        }
+
+        return authorities.toArray(new String[0]);
+    }
+
+}
